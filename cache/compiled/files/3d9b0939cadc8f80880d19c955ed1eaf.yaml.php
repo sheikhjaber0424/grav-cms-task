1c7085e1d12c97a4637ec77995960364e5e7f38f
sheikhjaber0424 <sheikhjaber190@gmail.com>
2024-05-07 17:06:45 +0600

    template.css file updated

---
+++ cache/compiled/files/3d9b0939cadc8f80880d19c955ed1eaf.yaml.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'D:/laragon/www/my-grav-site/user/data/feed/29811c6070379dd69bb820487301189e.yaml',
+    'modified' => 1715079888,
+    'size' => 1727,
+    'data' => NULL
+];
---
+++ cache/compiled/files/6ce4e8e961ce63ee0c6893ce8c37de4b.yaml.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'D:/laragon/www/my-grav-site/user/data/notifications/29811c6070379dd69bb820487301189e.yaml',
+    'modified' => 1715079887,
+    'size' => 7998,
+    'data' => NULL
+];
